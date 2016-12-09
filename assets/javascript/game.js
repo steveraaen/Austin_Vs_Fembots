45a307eab13989647508c5557aeecb800f556ca3
steveraaen <sraaen@gmail.com>
2016-12-09 11:54:42 -0500

    Added gif images of players figthing, removed test files, added comments.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,8 @@ var austin =
 	'attackpts': 15,
 	'strength': 200,
 	'defendpts': -40,	
-	'picture' : ('<img id = "ap" src ="./assets/images/austin.jpg">')
+	'picture' : ('<img id = "ap" src ="./assets/images/austin.jpg">'),
+	'altpicture' : ('<img id = "altap" src ="./assets/images/austin-powers.gif">')
 }
 var fembots =
  {
@@ -12,7 +13,8 @@ var fembots =
 	'strength': 300,
 	'attackpts': 30,
 	'defendpts': -35,
-	'picture' : ('<img id = "fem"src ="./assets/images/fembots.jpg">')
+	'picture' : ('<img id = "fem"src ="./assets/images/fembots.jpg">'),
+	'altpicture' : ('<img id = "altap" src ="./assets/images/fembots.gif">')
 }
 var drevil =
  {
@@ -20,7 +22,8 @@ var drevil =
 	'strength': 150,
 	'attackpts': 20,
 	'defendpts': -60,
-	'picture' : ('<img id = "de" src ="./assets/images/dr_evil.jpg">')
+	'picture' : ('<img id = "de" src ="./assets/images/dr_evil.jpg">'),
+	'altpicture' : ('<img id = "altap" src ="./assets/images/drevil.gif">')
 }
 var fatbastard =
 	{
@@ -28,7 +31,8 @@ var fatbastard =
 	'strength': 250,
 	'attackpts': 10,
 	'defendpts': -40,
-	'picture' : ('<img id = "fb" src ="./assets/images/fat_bastard.jpeg">')
+	'picture' : ('<img id = "fb" src ="./assets/images/fat_bastard.jpeg">'),
+	'altpicture' : ('<img id = "altap" src ="./assets/images/fat_bastard.gif">')
 };
 var players = [austin, fembots, drevil, fatbastard];
 var p1s = 0,p1a = 0,p1d = 0,p2s = 0,p2s = 0,p2s = 0, pl1, pl2, p1n, p2n ;
@@ -43,6 +47,7 @@ for(var i = 0; i < players.length; i++){
 		player.attr("data-defendpts", players[i].defendpts);
 		player.attr("data-strength", players[i].strength);
 		player.attr("data-pic", players[i].picture);
+		player.attr("data-altpic", players[i].altpicture);
 		player.text(player.data('name'))
 		p1n = player.text(player.data('strength'));
 
@@ -51,47 +56,50 @@ for(var i = 0; i < players.length; i++){
 //append new divs to top row
 		$("#start").append(player);
 }
-//make click handlers
+//make click handlers.  First one selects the clicked player and moves the others to the idle players section
 $('div').one('click', '.playerCont', function (){
 	$(this).removeClass('playerCont').addClass('playerOne')
 	$('.playerCont').not(this).appendTo('#idlePlayers').removeClass('playerCont').addClass('idlePlayers');
 	  pl1 = $(this);
 	  p1n = parseInt(pl1.data('strength')) + parseInt(pl1.data('attackpts'));
-	  console.log(pl1)
-});
+	});
+// Second one selects the enemy via function and runs the function
 function pick(){
 $('div').one('click', '.idlePlayers', function (){
 	$(this).appendTo('#enemy').removeClass('idlePlayers').addClass('enemy');
 	pl2 = $(this);
 	p2n = parseInt(pl2.data('strength')) + parseInt(pl2.data('attackpts'));
-	});
-};
+		});	
+	};
 pick();
 $('#idlePlayers').off('click', '.idlePlayers');
-$('div').on('click', '.action', function(){
-
+// Third handler calculates player strength and game results
+$('#clickHere').on('click', function(){
+// Update players' strength
 		p1n = p1n + parseInt(pl1.data('attackpts'));
 		p2n = p2n + parseInt(pl2.data('defendpts'));
-
-		var newPl1 = $('.playerOne').data('name') + $('.playerOne').data('pic') + p1n;
-		var newPl2 = $('.enemy').data('name') + $('.enemy').data('pic') + p2n;
+// Redfines players' objects with new strength and action (gif) images.
+		var newPl1 = $('.playerOne').data('name') + $('.playerOne').data('altpic') + p1n;
+		var newPl2 = $('.enemy').data('name') + $('.enemy').data('altpic') + p2n;
+// Appends players' objects to the screen
 			$('.playerOne').html(newPl1) ;
 			$('.enemy').html(newPl2); 
 			$('#enemyStatus').html("You attacked  " + $('.enemy').data('name') + "  for <b> " + $('.enemy').data('defendpts') + "</b>  damage. <br>" +
 			$('.enemy').data('name') + "  attacked you back for <b> " + $('.playerOne').data('defendpts') + "</b>  damage.");
-		console.log(p2n)
-
+// Tracks players' status. If player 1 wins, run the pick function again
 		if(p2n <= 0){
 			$('#enemy').html('');
-			$('#enemyStatus').html("You won.  Pick another opponent")
+			$('#enemyStatus').html("You won.  Pick another opponent");
 			pick();
+/*			if ($('#idlePlayers').hasClass('.idlePlayers') === false){
+				alert("Yeah Baby Yeah!");
+			}*/
 		}
-
 		else if(p1n <= 0){
 			$('#enemyStatus').html("You have been defeted.  Game Over")
 			$('#replay').append('<button>Replay</button>')
 		}
-		
+
 		});
 });
 
